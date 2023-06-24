999b21fcad82c80499d0374f5304e90fc97c8467
ShreetamMishra <shreetammishra1@gmail.com>
2023-06-24 23:49:47 +0530

    Adding error reporting

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,9 +1,42 @@
-export function onError(error) {
-    let message = error.toString();
-    // Auth errors
-    if (!(error instanceof Error) && error.message) {
-      message = error.message;
+import * as Sentry from "@sentry/browser";
+
+const isLocal = process.env.NODE_ENV === "development";
+//const isLocal = false;
+
+export function initSentry() {
+    if (isLocal) {
+        return;
     }
+
+    Sentry.init({
+        dsn: "https://e217f43d9d8847b09e861afd62396975@o4505415801372672.ingest.sentry.io/4505415813627904",
+    });
+}
+
+export function logError(error, errorInfo = null) {
+    if (isLocal) {
+        return;
+    }
+
+    Sentry.withScope((scope) => {
+        errorInfo && scope.setExtras(errorInfo);
+        Sentry.captureException(error);
+    });
+}
+
+export function onError(error) {
+    let errorInfo = {};
+    let message = error.toString();
+
+    if (!(error instanceof Error) && error.message) {
+        errorInfo = error;
+        message = error.message;
+        error = new Error(message);
+    } else if (error.config && error.config.url) {
+        errorInfo.url = error.config.url;
+    }
+
+    logError(error, errorInfo);
+
     alert(message);
-  }
-  
+}
