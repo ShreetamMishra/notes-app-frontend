f61ea290f691ba832f5f6c53c79adb52ca95f71a
ShreetamMishra <shreetammishra1@gmail.com>
2023-06-25 15:17:26 +0530

    reset password

--- src/Routes.js
+++ src/Routes.js
@@ -9,6 +9,7 @@ import Notes from "./containers/Notes";
 import Settings from "./containers/Settings";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
+import ResetPassword from "./containers/ResetPassword";
 
 export default function Routes() {
   return (
@@ -16,22 +17,24 @@ export default function Routes() {
       <Route exact path="/">
        <Home />
       </Route>
-
+      <UnauthenticatedRoute exact path="/login/reset">
+        <ResetPassword />
+      </UnauthenticatedRoute>
       <UnauthenticatedRoute exact path="/login">
         <Login />
       </UnauthenticatedRoute>
       <UnauthenticatedRoute exact path="/signup">
         <Signup />
       </UnauthenticatedRoute>
-    <AuthenticatedRoute exact path="/settings">
-       <Settings />
-    </AuthenticatedRoute>
-    <AuthenticatedRoute exact path="/notes/new">
-      <NewNote />
-    </AuthenticatedRoute>
-    <AuthenticatedRoute exact path="/notes/:id">
-      <Notes />
-    </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/settings">
+        <Settings />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/new">
+        <NewNote />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/:id">
+        <Notes />
+      </AuthenticatedRoute>
 
 
       {/* Finally, catch all unmatched routes */}
