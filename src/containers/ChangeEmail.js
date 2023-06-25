5d267a1f9c455ad0cbb8481a67ce49e1c34545b8
ShreetamMishra <122048171+ShreetamMishra@users.noreply.github.com>
2023-06-25 16:48:58 +0530

    Update ChangeEmail.js

--- src/containers/ChangeEmail.js
+++ src/containers/ChangeEmail.js
@@ -6,7 +6,6 @@ import {
   FormGroup,
   FormControl,
   FormLabel,
-  Button,
 } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import { useFormFields } from "../libs/hooksLib";
