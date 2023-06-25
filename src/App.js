d291ee368c52936a191cd34dd24bafb57efe4b0c
ShreetamMishra <122048171+ShreetamMishra@users.noreply.github.com>
2023-06-25 23:47:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,21 +17,21 @@ function App() {
   const history = useHistory();
   
 
-  useEffect(() => {
+ useEffect(() => {
     onLoad();
   }, []);
   
-  async function onLoad() {
-    try {
-      await Auth.currentSession();
-      userHasAuthenticated(true);
-    } catch (e) {
-      if (e !== "No current user") {
-        onError(e);
-      }
-    }
-    setIsAuthenticating(false);
-  }
+  // async function onLoad() {
+  //   try {
+  //     await Auth.currentSession();
+  //     userHasAuthenticated(true);
+  //   } catch (e) {
+  //     if (e !== "No current user") {
+  //       onError(e);
+  //     }
+  //   }
+  //   setIsAuthenticating(false);
+  // }
   async function onLoad() {
     loadFacebookSDK();
     try {
