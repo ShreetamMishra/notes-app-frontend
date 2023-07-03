af0b4b2635f28ac9675337461afb9a2f4a9797f4
ShreetamMishra <122048171+ShreetamMishra@users.noreply.github.com>
2023-07-03 17:19:48 +0530

    Update Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,95 +1,97 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
 import "./Home.css";
+import { API } from "aws-amplify";
 import { BsPencilSquare } from "react-icons/bs";
 import { LinkContainer } from "react-router-bootstrap";
 import { Link } from "react-router-dom";
 import heroImage from "./image/bubble.png";
-import { API, Storage } from "aws-amplify";
 
 export default function Home() {
-  const [setNotes] = useState([]);
+  const [notes, setNotes] = useState([]);
   const { isAuthenticated } = useAppContext();
   const [isLoading, setIsLoading] = useState(true);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [filteredNotes, setFilteredNotes] = useState([]);
-
-  const loadNotes = useCallback(async () => {
-    const response = await API.get("notes", "/notes");
-    const notesWithAttachmentURL = await Promise.all(
-      response.map(async (note) => {
-        if (note.attachment) {
-          const attachmentURL = await Storage.vault.get(note.attachment);
-          return { ...note, attachmentURL };
-        }
-        return note;
-      })
-    );
-
-    const filteredNotes = notesWithAttachmentURL.filter((note) =>
-      note.content && note.content.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-
-    setFilteredNotes(filteredNotes);
-
-    return notesWithAttachmentURL;
-  }, [searchQuery]);
-
- useEffect(() => {
-  async function onLoad() {
-    if (!isAuthenticated) {
-      return;
+  useEffect(() => {
+    async function onLoad() {
+      if (!isAuthenticated) {
+        return;
+      }
+      try {
+        const notes = await loadNotes();
+        setNotes(notes);
+      } catch (e) {
+        onError(e);
+      }
+      setIsLoading(false);
     }
-    try {
-      const notes = await loadNotes();
-      setNotes(notes);
-    } catch (e) {
-      onError(e);
-    }
-    setIsLoading(false);
+    onLoad();
+  }, [isAuthenticated]);
+  
+  function loadNotes() {
+    return API.get("notes", "/notes");
   }
-  onLoad();
-}, [isAuthenticated, loadNotes, setNotes]);
-
-
+  
   function renderNotesList(notes) {
     return (
-      <div className="notes-container">
-        <LinkContainer to="/notes/new">
-          <ListGroup.Item action className="py-3 text-nowrap text-truncate custom-note-item custom-note-new">
+      <>
+        {/* <LinkContainer to="/notes/new">
+          <ListGroup.Item action className="py-3 text-nowrap text-truncate custom-note-item">
             <BsPencilSquare size={17} />
             <span className="ml-2 font-weight-bold custom-note-text">Create a new note</span>
           </ListGroup.Item>
         </LinkContainer>
+        {notes.map(({ noteId, content, createdAt }) => (
+          <LinkContainer key={noteId} to={`/notes/${noteId}`}>
+            <ListGroup.Item action> 
+            
+              <span className="font-weight-bold">
+                {content.trim().split("\n")[0]}
+              </span>
+              <br />
+              <span className="text-muted">
+                Created: {new Date(createdAt).toLocaleString()}
+              </span>
+            </ListGroup.Item>
+          </LinkContainer>
+          
+        ))} */}
+        <div className="notes-container">
+  <LinkContainer to="/notes/new">
+    <ListGroup.Item action className="py-3 text-nowrap text-truncate custom-note-item custom-note-new">
+      <BsPencilSquare size={17} />
+      <span className="ml-2 font-weight-bold custom-note-text">Create a new note</span>
+    </ListGroup.Item>
+  </LinkContainer>
+  {/* Sort the notes by createdAt in descending order latest one is in the first */}
+  {notes.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)) 
+    .map(({ noteId, content, createdAt }, index) => (
+    <LinkContainer key={noteId} to={`/notes/${noteId}`}>
+      <ListGroup.Item action className="custom-note-item">
+        <span className="font-weight-bold">
+          {content.trim().split("\n")[0]}
+        </span>
+        <br />
+        <span className="text-muted">
+          Created: {new Date(createdAt).toLocaleString()}
+        </span>
+      </ListGroup.Item>
+    </LinkContainer>
+  ))}
+</div>
 
-        {/* Sort the notes by createdAt in descending order latest one is in the first */}
-        {notes
-          .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-          .map(({ noteId, content, createdAt, attachmentURL }) => (
-            <LinkContainer key={noteId} to={`/notes/${noteId}`}>
-              <ListGroup.Item action className="custom-note-item">
-                {attachmentURL && <img src={attachmentURL} alt={`Note Attachment ${noteId}`} />}
-                <span className="font-weight-bold">{content.trim().split("\n")[0]}</span>
-                <br />
-                <span className="text-muted">
-                  Created: {new Date(createdAt).toLocaleString()}
-                </span>
-              </ListGroup.Item>
-            </LinkContainer>
-          ))}
-      </div>
+
+
+      </>
     );
   }
-
+  
   function renderLander() {
     return (
       <div className="lander">
         <h1>Notes</h1>
-        <p className="typewriter" style={{ color: "black" }}>
-          A simple note taking app
-        </p>
+        <p className="typewriter" style={{ color: 'black' }}>A simple note taking app</p>
         <div className="pt-3">
           <Link to="/login" className="btn btn-info btn-lg mr-3">
             Login
@@ -99,42 +101,36 @@ export default function Home() {
           </Link>
         </div>
         <div className="hero">
-          <img src={heroImage} alt="bubble" />
-          <img src={heroImage} alt="bubble" />
-          <img src={heroImage} alt="bubble" />
-          <img src={heroImage} alt="bubble" />
-          <img src={heroImage} alt="bubble" />
-          <img src={heroImage} alt="bubble" />
-          <img src={heroImage} alt="bubble" />
-          <img src={heroImage} alt="bubble" />
-          <img src={heroImage} alt="bubble" />
+        <img src={heroImage} alt="bubble" />
+      <img src={heroImage} alt="bubble" />
+      <img src={heroImage} alt="bubble" />
+      <img src={heroImage} alt="bubble" />
+      <img src={heroImage} alt="bubble" />
+      <img src={heroImage} alt="bubble" />
+      <img src={heroImage} alt="bubble" />
+      <img src={heroImage} alt="bubble" />
+      <img src={heroImage} alt="bubble" />
         </div>
       </div>
     );
   }
+  
 
   function renderNotes() {
     return (
       <div className="notes">
         <h2 className="pb-3 mt-4 mb-3 border-bottom">Your Notes</h2>
-
-        <div className="search-container">
-          <input
-            type="text"
-            placeholder="Search notes..."
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-        </div>
-
-        <ListGroup>{!isLoading && renderNotesList(filteredNotes)}</ListGroup>
+        <ListGroup>{!isLoading && renderNotesList(notes)}</ListGroup>
       </div>
     );
   }
 
   return (
     <div className="Home">
+      
       {isAuthenticated ? renderNotes() : renderLander()}
+      
     </div>
+   
   );
 }
